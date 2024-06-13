8ce6383550abe51fc5df4d6bbdbcc94fa8a933a1
brokenfdreams <brokenfdreams@protonmail.com>
2024-06-13 21:00:03 +0200

    added lintr and fixed api call

--- src/components/learn/LearnPage.tsx
+++ src/components/learn/LearnPage.tsx
@@ -3,7 +3,7 @@ import {useBackButton, useMainButton} from "@tma.js/sdk-react";
 import axios from "axios";
 import React, {FC, useEffect, useState} from "react";
 
-const URL = "http://127.0.0.1:8080/learning/web/"
+const URL = 'http://10.164.16.83:8080/';
 
 export const LearnPage: FC = () => {
     const [cards, setCards] = useState<LearningCard[]>([])
@@ -17,14 +17,16 @@ export const LearnPage: FC = () => {
 
     const {isLoading, data, error} = useQuery<LearningCard[]>({
         queryKey: ['learningCards'],
-        queryFn: () => axios.get(URL + 'learning/cards/en/for/ru')
+        queryFn: () => axios.get(URL + 'learning/web/cards/error', {
+            headers: {
+                "Accept": "application/json",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Connection": "keep-alive"
+            }
+        })
             .then((response) => {
-                console.log(`response: ${response}`)
+                console.log(`response: ${response.data}`)
                 return response.data
-            })
-            .catch((e) => {
-                console.log(e);
-                return e;
             })
     });
 
@@ -33,9 +35,9 @@ export const LearnPage: FC = () => {
             <div>Card to learn</div>
             <div>Data: {data?.toString()}</div>
             <div>Error: {error?.toString()}</div>
-            <div>Cards: {cards.toString()}</div>
+            <div>Cards:</div>
             <ul>
-                {cards.map(card => <li key={card.id.toString()}>{card.sentence}</li>)}
+                {data?.map(card => <li key={card.id.toString()}>{card.sentence}</li>)}
             </ul>
         </>
     );
@@ -47,8 +49,8 @@ class LearningCard {
         public readonly sentence: String,
         public readonly learningPhrase: String,
         public readonly learningPhraseTranslation: String,
-        public readonly sentenceTranslation: String,
-        public readonly sentenceWordsTranslation: String,
+        public readonly nativeSentenceTranslation: String,
+        public readonly literalSentenceTranslation: String,
         public readonly tips: String[],
         public readonly possiblePhrases: String[]
     ) {
